0514c1d3930a91dc5f688e38143d3b2df459757b
Leo Marques <marquessilva600@gmail.com>
2026-01-23 15:50:29 -0300

    fix: atualiza tipo Field.tag para incluir todas as tags usadas

--- src/types/index.ts
+++ src/types/index.ts
@@ -5,7 +5,7 @@ export interface Field {
   instruction: string
   example: string
   required?: boolean
-  tag?: 'CP' | 'CC' // Campos consolidados ou do contrato
+  tag?: 'CP' | 'CC' | 'CE' | 'CA' | 'CADASTRO' | 'FINANCEIRO' // Tags para identificação de campos por etapa
 }
 
 export interface ChecklistItem {
